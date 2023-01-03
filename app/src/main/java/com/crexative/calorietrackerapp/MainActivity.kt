910e4c933ad3f718b75ee226599f5b16b40e7484
Johan Mosquera <joncamilomoni@gmail.com>
2023-01-03 13:24:41 -0500

    Navigation Util

--- app/src/main/java/com/crexative/calorietrackerapp/MainActivity.kt
+++ app/src/main/java/com/crexative/calorietrackerapp/MainActivity.kt
@@ -3,13 +3,11 @@ package com.crexative.calorietrackerapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.crexative.calorietrackerapp.navigation.navigate
+import com.crexative.core.navigation.Route
 import com.crexative.calorietrackerapp.ui.theme.CalorieTrackerAppTheme
 import com.crexative.onboarding_presentation.welcome.WelcomeScreen
 
@@ -18,7 +16,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CalorieTrackerAppTheme {
-                WelcomeScreen()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = Route.WELCOME) {
+                    composable(Route.WELCOME){
+                        WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(Route.AGE){}
+                    composable(Route.GENDER){}
+                    composable(Route.HEIGHT){}
+                    composable(Route.WEIGHT){}
+                    composable(Route.NUTRIENT_GOAL){}
+                    composable(Route.ACTIVITY){}
+                    composable(Route.GOAL){}
+                    composable(Route.TRACKER_OVERVIEW){}
+                    composable(Route.SEARCH){}
+                }
             }
         }
     }

30d64bf2dc69230bbe075f64937f89bf5e5d0813
Johan Mosquera <joncamilomoni@gmail.com>
2023-01-03 11:20:48 -0500

    Welcome Screen and Utility for dimensions in compose

--- app/src/main/java/com/crexative/calorietrackerapp/MainActivity.kt
+++ app/src/main/java/com/crexative/calorietrackerapp/MainActivity.kt
@@ -11,13 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.crexative.calorietrackerapp.ui.theme.CalorieTrackerAppTheme
+import com.crexative.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalorieTrackerAppTheme {
-
+                WelcomeScreen()
             }
         }
     }

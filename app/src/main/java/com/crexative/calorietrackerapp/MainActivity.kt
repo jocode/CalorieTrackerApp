7484c46f4221c121583a5a2739b547f316e44215
Johan Mosquera <joncamilomoni@gmail.com>
2023-01-03 17:53:55 -0500

    Age Screen
    For the Age Screen is created the ViewModel and one UseCase saved in the core module to filter the digits for the age.

--- app/src/main/java/com/crexative/calorietrackerapp/MainActivity.kt
+++ app/src/main/java/com/crexative/calorietrackerapp/MainActivity.kt
@@ -1,40 +1,61 @@
 package com.crexative.calorietrackerapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.crexative.calorietrackerapp.navigation.navigate
 import com.crexative.core.navigation.Route
 import com.crexative.calorietrackerapp.ui.theme.CalorieTrackerAppTheme
+import com.crexative.onboarding_presentation.age.AgeScreen
 import com.crexative.onboarding_presentation.gender.GenderScreen
 import com.crexative.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalorieTrackerAppTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = Route.WELCOME) {
-                    composable(Route.WELCOME){
-                        WelcomeScreen(onNavigate = navController::navigate)
+                val scaffoldState = rememberScaffoldState()
+                
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
+                ) {
+
+                    NavHost(navController = navController, startDestination = Route.WELCOME) {
+                        composable(Route.WELCOME){
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.AGE){
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.GENDER){
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.HEIGHT){}
+                        composable(Route.WEIGHT){}
+                        composable(Route.NUTRIENT_GOAL){}
+                        composable(Route.ACTIVITY){}
+                        composable(Route.GOAL){}
+                        composable(Route.TRACKER_OVERVIEW){}
+                        composable(Route.SEARCH){}
                     }
-                    composable(Route.AGE){}
-                    composable(Route.GENDER){
-                        GenderScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.HEIGHT){}
-                    composable(Route.WEIGHT){}
-                    composable(Route.NUTRIENT_GOAL){}
-                    composable(Route.ACTIVITY){}
-                    composable(Route.GOAL){}
-                    composable(Route.TRACKER_OVERVIEW){}
-                    composable(Route.SEARCH){}
+                    
                 }
             }
         }

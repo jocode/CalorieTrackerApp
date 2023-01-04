895de86cf2641b4ca9b4736e29062c7fff51a492
Johan Mosquera <joncamilomoni@gmail.com>
2023-01-04 17:51:40 -0500

    Tracker Use Cases

---
+++ tracker/tracker_domain/src/main/java/com/crexative/tracker_domain/di/TrackerDomainModule.kt
@@ -0,0 +1,31 @@
+package com.crexative.tracker_domain.di
+
+import com.crexative.core.domain.preferences.Preferences
+import com.crexative.tracker_domain.repository.TrackerRepository
+import com.crexative.tracker_domain.use_case.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object TrackerDomainModule {
+
+    @Provides
+    @ViewModelScoped
+    fun provideTrackerUseCases(
+        repository: TrackerRepository,
+        preferences: Preferences
+    ) : TrackerUseCases {
+        return TrackerUseCases(
+            trackFood = TrackFood(repository),
+            searchFood = SearchFood(repository),
+            getFoodsForDate = GetFoodsForDate(repository),
+            deleteTrackedFood = DeleteTrackedFood(repository),
+            calculateMealNutrients = CalculateMealNutrients(preferences)
+        )
+    }
+
+}

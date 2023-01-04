e057d939fde9ff829ea6a13be218c5bd561bdbce
Johan Mosquera <joncamilomoni@gmail.com>
2023-01-04 15:10:36 -0500

    Setting Up the Local DB

---
+++ tracker/tracker_data/src/main/java/com/crexative/tracker_data/di/TrackerDataModule.kt
@@ -0,0 +1,46 @@
+package com.crexative.tracker_data.di
+
+import com.crexative.tracker_data.remote.OpenFoodApi
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TrackerDataModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(OpenFoodApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOpenFoodApi(retrofit: Retrofit) : OpenFoodApi {
+        return retrofit.create(OpenFoodApi::class.java)
+    }
+
+}
---
+++ tracker/tracker_data/src/main/java/com/crexative/tracker_data/remote/dto/SearchDto.kt
@@ -0,0 +1,5 @@
+package com.crexative.tracker_data.remote.dto
+
+data class SearchDto(
+    val products: List<Product>
+)

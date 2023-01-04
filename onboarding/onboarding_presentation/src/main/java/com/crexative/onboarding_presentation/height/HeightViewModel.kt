c9f83dfd9d374f559b560019d8fc6710bf9a7059
Johan Mosquera <joncamilomoni@gmail.com>
2023-01-04 11:36:16 -0500

    Height & weight Screen and ViewModels

---
+++ onboarding/onboarding_presentation/src/main/java/com/crexative/onboarding_presentation/height/HeightViewModel.kt
@@ -0,0 +1,53 @@
+package com.crexative.onboarding_presentation.height
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.crexative.core.domain.preferences.Preferences
+import com.crexative.core.use_case.FilterOutDigits
+import com.crexative.core.util.UiEvent
+import com.crexative.core.util.UiText
+import com.crexative.core.R
+import com.crexative.core.navigation.Route
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HeightViewModel @Inject constructor(
+    private val preferences: Preferences,
+    private val filterOutDigits: FilterOutDigits
+) : ViewModel() {
+
+    var height by mutableStateOf("170")
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onHeightEnter(height: String) {
+        if (height.length <= 3 ) {
+            this.height = filterOutDigits(height)
+        }
+    }
+
+    fun onNextClick() {
+        viewModelScope.launch {
+            val heightNumber = height.toIntOrNull() ?: run {
+                _uiEvent.send(
+                    UiEvent.ShowSnackBar(
+                        UiText.StringResource(R.string.error_height_cant_be_empty)
+                    )
+                )
+                return@launch
+            }
+            preferences.saveHeight(heightNumber)
+            _uiEvent.send(UiEvent.Navigate(Route.WEIGHT))
+        }
+    }
+
+}

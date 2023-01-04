91c17c38f3ea8c461ee7819ee2f8bbd4ab98d7ab
Johan Mosquera <joncamilomoni@gmail.com>
2023-01-04 15:29:09 -0500

    Setting Up the Local DB
    The DI fot localDatabase is created inside of the module trackerData, because this package have all the implementation for this feature

---
+++ tracker/tracker_data/src/main/java/com/crexative/tracker_data/local/TrackerDatabase.kt
@@ -0,0 +1,15 @@
+package com.crexative.tracker_data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.crexative.tracker_data.local.entity.TrackedFoodEntity
+
+@Database(
+    entities = [TrackedFoodEntity::class],
+    version = 1
+)
+abstract class TrackerDatabase: RoomDatabase() {
+
+    abstract fun trackerDao(): TrackerDao
+
+}

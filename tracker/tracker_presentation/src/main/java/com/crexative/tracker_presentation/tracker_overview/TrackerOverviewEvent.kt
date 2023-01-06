789dcbda50834358577da20bf4853e0401da6625
Johan Mosquera <joncamilomoni@gmail.com>
2023-01-06 14:54:36 -0500

    TrackerOverviewViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/crexative/tracker_presentation/tracker_overview/TrackerOverviewEvent.kt
@@ -0,0 +1,11 @@
+package com.crexative.tracker_presentation.tracker_overview
+
+import com.crexative.tracker_domain.model.TrackedFood
+
+sealed class TrackerOverviewEvent {
+    object OnNextDayClick: TrackerOverviewEvent()
+    object OnPreviousDayClick: TrackerOverviewEvent()
+    data class OnToggleMealClick(val meal: Meal): TrackerOverviewEvent()
+    data class OnDeleteTrackedFood(val trackedFood: TrackedFood): TrackerOverviewEvent()
+    data class OnAddFoodClick(val meal: Meal): TrackerOverviewEvent()
+}

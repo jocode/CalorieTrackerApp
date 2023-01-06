c90d6585b0291e6fe1c1bc0817c12e019ad7f9b0
Johan Mosquera <joncamilomoni@gmail.com>
2023-01-06 14:54:36 -0500

    TrackerOverviewViewModel

--- core/src/main/java/com/crexative/core/data/preferences/DefaultPreferences.kt
+++ core/src/main/java/com/crexative/core/data/preferences/DefaultPreferences.kt
@@ -88,4 +88,17 @@ class DefaultPreferences(
             fatRatio = fatRatio
         )
     }
+
+    override fun saveShouldShowOnboarding(shouldShow: Boolean) {
+        sharedPref.edit()
+            .putBoolean(Preferences.KEY_SHOULD_SHOW_ONBOARDIND, shouldShow)
+            .apply()
+    }
+
+    override fun loadShouldShowOnboarding(): Boolean {
+        return sharedPref.getBoolean(
+            Preferences.KEY_SHOULD_SHOW_ONBOARDIND,
+            true
+        )
+    }
 }

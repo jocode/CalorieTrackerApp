9d346d36802f12fbcac5e8d6e60d72b5cbe9cd42
Johan Mosquera <joncamilomoni@gmail.com>
2023-01-10 16:27:25 -0500

    Nutrient Header UI

---
+++ tracker/tracker_presentation/src/main/java/com/crexative/tracker_presentation/tracker_overview/components/NutrientsBar.kt
@@ -0,0 +1,98 @@
+package com.crexative.tracker_presentation.tracker_overview.components
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.foundation.Canvas
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Size
+import com.crexative.core_ui.CarbColor
+import com.crexative.core_ui.FatColor
+import com.crexative.core_ui.ProteinColor
+
+@Composable
+fun NutrientsBar(
+    carbs: Int,
+    protein: Int,
+    fat: Int,
+    calories: Int,
+    calorieGoal: Int,
+    modifier: Modifier = Modifier
+) {
+    val background = MaterialTheme.colors.background
+    val caloriesExceededColor = MaterialTheme.colors.error
+
+    val carbWidthRation = remember {
+        Animatable(0f)
+    }
+    val proteinWithRatio = remember {
+        Animatable(0f)
+    }
+    val fatWidthRatio = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = carbs) {
+        carbWidthRation.animateTo(
+            targetValue = ((carbs * 4f) / calorieGoal)
+        )
+    }
+    LaunchedEffect(key1 = protein) {
+        proteinWithRatio.animateTo(
+            targetValue = ((protein * 4f) / calorieGoal)
+        )
+    }
+    LaunchedEffect(key1 = fat) {
+        fatWidthRatio.animateTo(
+            targetValue = ((fat * 9f) / calorieGoal)
+        )
+    }
+
+    Canvas(modifier = modifier) {
+        if (calories <= calorieGoal) {
+            val carbsWidth = carbWidthRation.value * size.width
+            val proteinWidth = proteinWithRatio.value * size.width
+            val fatWidth = fatWidthRatio.value * size.width
+
+            drawRoundRect(
+                color = background,
+                size = size,
+                cornerRadius = CornerRadius(100f)
+            )
+            drawRoundRect(
+                color = FatColor,
+                size = Size(
+                    width = carbsWidth + proteinWidth + fatWidth,
+                    height = size.height
+                ),
+                cornerRadius = CornerRadius(100f)
+            )
+            drawRoundRect(
+                color = ProteinColor,
+                size = Size(
+                    width = carbsWidth + proteinWidth,
+                    height = size.height
+                ),
+                cornerRadius = CornerRadius(100f)
+            )
+            drawRoundRect(
+                color = CarbColor,
+                size = Size(
+                    width = carbsWidth,
+                    height = size.height
+                ),
+                cornerRadius = CornerRadius(100f)
+            )
+        } else {
+            drawRoundRect(
+                color = caloriesExceededColor,
+                size = size,
+                cornerRadius = CornerRadius(100f)
+            )
+        }
+    }
+
+}
